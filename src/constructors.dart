ad3ef8b4121d035735bf2f746c829468abbf756c
陈飞跃 <nulibj@163.com>
2019-05-05 15:59:28 +0800

    [Add]

---
+++ src/constructors.dart
@@ -0,0 +1,42 @@
+void main() {
+  Person p = Person.withName("zhangsan");
+  print(p.name);
+
+  Person person;
+  // ? 不为 null 则执行
+  person?.work();
+  person = new Person("zahng ", 15);
+
+  if (person is Person) {
+    print(person.name);
+  }
+  // .. 级联操作符
+  person..name = "TOM"..age = 20..work();
+}
+
+class Person {
+  String name;
+  int age;
+
+//  Person(){}
+  Person(this.name, this.age) {
+    print(name);
+  }
+
+  Person.withName(name) {
+    this.name = name;
+  }
+
+  Person.withAge(age) {
+    this.age = age;
+  }
+
+  work() {
+    print("name = $name, age = $age");
+  }
+
+  @override
+  String toString() {
+    return super.toString();
+  }
+}
