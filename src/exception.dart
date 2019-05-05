abd326958b8e1ee1268f7adb2c4d8d940e659e2b
陈飞跃 <nulibj@163.com>
2019-05-05 15:59:43 +0800

    [Add]

---
+++ src/exception.dart
@@ -0,0 +1,10 @@
+void main() {
+  try {
+    // ···
+  } on Exception catch (e) {
+    print('Exception details:\n $e');
+  } catch (e, s) {
+    print('Exception details:\n $e');
+    print('Stack trace:\n $s');
+  }
+}
