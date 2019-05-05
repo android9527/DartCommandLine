25a7c5f53ded9fed3c76633de51e7952ab51b424
陈飞跃 <nulibj@163.com>
2019-05-05 16:01:29 +0800

    [Add]

---
+++ src/string.dart
@@ -0,0 +1,19 @@
+void main() {
+  print(s1*3);
+}
+
+String s1 = 'aaa\'';
+String s2 = "aaa'";
+// 多行字符串
+String s3 = """Hello
+Dart""";
+// 原字符串
+String s4 = r"aaa\n";
+String s5 = "s1 = $s1, 1 + 2 = ${1 + 2}";
+
+bool b = true;
+
+// 这是⼀个⾏注释
+/* 这是⼀个多⾏的
+ 块注释。 */
+/* /* */ */
