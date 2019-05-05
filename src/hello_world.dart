6375c382940477312bd720322a0dbe4e9da448df
陈飞跃 <nulibj@163.com>
2019-05-05 15:57:27 +0800

    [Add]add hello_word.dart

---
+++ src/hello_world.dart
@@ -0,0 +1,44 @@
+void main(List name) {
+  print(name);
+
+  var a;
+  a = 10;
+  a = "aaa";
+  print("s2 = $a");
+  print("1 + 2 = ${1 + 2}");
+
+  print("""
+/** /**
+yj\n*/ */
+    """);
+
+// 这是⼀个⾏注释
+  /* 这是⼀个多⾏的
+     块注释。 */
+  /* /*
+   */
+   * */
+  var result = printPerson("zhangsan", 18);
+  print(result);
+  print(printPerson("zhangsan", 18));
+
+  Function function = getPerson;
+  function("zhangsan", 18);
+
+  var func = (str) => str;
+  print(func("test"));
+}
+
+final c = 0;
+
+class Test {
+  static var b = 1;
+}
+
+String getPerson(String name, int age) {
+  return "name = $name, age = $age";
+}
+printPerson(name, age) {
+  print("name = $name, age = $age");
+}
+//getPerson(name, age) => "name = $name, age = $age";
